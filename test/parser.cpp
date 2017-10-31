33d8b69ad88a4cd75c5705ee1b4e788429de8e30
Simon Pettersson <u4007150@C0025311.getingegroup.local>
2017-10-31 17:36:40 +0100

    Added tests for parser

--- test/parser.cpp
+++ test/parser.cpp
@@ -3,156 +3,292 @@
 #include <cx_parser.h>
 
 SCENARIO("Character matching") {
-  
+  using cx::parser::bind;
+  using cx::parser::combine;
+  using cx::parser::fmap;
+  using cx::parser::lift;
+  using cx::parser::make_char_parser;
+  using cx::parser::many1;
+  using cx::parser::many;
+  using cx::parser::one_of;
+  using cx::parser::operator|;
+  using cx::parser::option;
+  using cx::parser::parse_input_t;
+  using std::string_view;
+
   GIVEN("make_char_parser('a')") {
-    constexpr auto match = cx::parser::make_char_parser('a');
-    THEN("it should match 'a'") {
-      CHECK( match("a") );
+    constexpr auto match = make_char_parser('a');
+    THEN("it should match \"a\"") {
+      constexpr auto result = match("a");
+      REQUIRE( result );
+      CHECK( result->first == 'a' );
+      CHECK( result->second == "" );
     }
-    THEN("it should NOT match 'b'") {
-      CHECK( !match("b") );
+    THEN("it should NOT match \"b\"") {
+      constexpr auto result = match("b");
+      CHECK( !result );
+    }
+    THEN("it should NOT match \"\"") {
+      constexpr auto result = match("");
+      CHECK( !result );
     }
   }
      
   GIVEN("a character matcher [one_of(\"abc\")]") {
-    constexpr auto match = cx::parser::one_of("abc");
+    constexpr auto match = one_of("abc");
     THEN("it should match \"a\"") {
-      CHECK( match("a") );
+      constexpr auto result = match("a");
+      REQUIRE( result );
+      CHECK( result->first == 'a' );
+      CHECK( result->second == "" );
     }
     THEN("it should match \"b\"") {
-      CHECK( match("b") );
+      constexpr auto result = match("b");
+      REQUIRE( result );
+      CHECK( result->first == 'b' );
+      CHECK( result->second == "" );
     }
     THEN("it should match \"c\"") {
-      CHECK( match("c") );
+      constexpr auto result = match("c");
+      REQUIRE( result );
+      CHECK( result->first == 'c' );
+      CHECK( result->second == "" );
     }
     THEN("it should NOT match \"d\"") {
-      CHECK( !match("d") );
+      constexpr auto result = match("d");
+      CHECK( !result );
     }    
+    THEN("it should NOT match \"\"") {
+      constexpr auto result = match("");
+      CHECK( !result );
+    }
   }
 
-  // GIVEN("a character matcher [exactly('a') | exactly('k')]") {
-  //   constexpr auto match = exactly('a') | exactly('b');
-  //   THEN("it should match \"a\"") {
-  //     CHECK( match("a") );
-  //   }
-  //   THEN("it should match \"b\"") {
-  //     CHECK( match("b") );
-  //   }
-  //   THEN("it should NOT match \"c\"") {
-  //     CHECK( !match("c") );
-  //   }
-  // }
+  GIVEN("a character matcher [make_char_parser('a') | make_char_parser('k')]") {
+    constexpr auto match = make_char_parser('a') | make_char_parser('b');
+    THEN("it should match \"a\"") {
+      constexpr auto result = match("a");
+      CHECK( result);
+    }
+    THEN("it should match \"b\"") {
+      constexpr auto result = match("b");
+      CHECK( result );
+    }
+    THEN("it should NOT match \"c\"") {
+      constexpr auto result = match("c");
+      CHECK( !result );
+    }
+    THEN("it should NOT match \"\"") {
+      constexpr auto result = match("");
+      CHECK( !result );
+    }
+  }
 
-  // GIVEN("a character matcher [one_of(\"ab\") | one_of(\"bc\"]") {
-  //   constexpr auto match = one_of("ab") | one_of("bc");
-  //   THEN("it should match \"a\"") {
-  //     CHECK( match("a") );
-  //   }
-  //   THEN("it should match \"b\"") {
-  //     CHECK( match("b") );
-  //   }
-  //   THEN("it should match \"c\"") {
-  //     CHECK( match("c") );
-  //   }
-  //   THEN("it should NOT match \"d\"") {
-  //     CHECK( !match("d") );
-  //   }
-  // }
+  GIVEN("a character matcher [one_of(\"ab\") | one_of(\"bc\"]") {
+    constexpr auto match = one_of("ab") | one_of("bc");
+    THEN("it should match \"a\"") {
+      constexpr auto result = match("a");
+      REQUIRE( result );
+      CHECK( result->first == 'a' );
+      CHECK( result->second == "" );
+    }
+    THEN("it should match \"b\"") {
+      constexpr auto result = match("b");
+      REQUIRE( result );
+      CHECK( result->first == 'b' );
+      CHECK( result->second == "" );
+    }
+    THEN("it should match \"c\"") {
+      constexpr auto result = match("c");
+      REQUIRE( result );
+      CHECK( result->first == 'c' );
+      CHECK( result->second == "" );
+    }
+    THEN("it should NOT match \"d\"") {
+      constexpr auto result = match("d");
+      CHECK( !result );
+    }
+    THEN("it should NOT match \"\"") {
+      constexpr auto result = match("");
+      CHECK( !result );
+    }
+  }
   
-  // GIVEN("a character matcher [fmap(one_of(\"ab\"), toupper]") {
-  //   constexpr auto toupper = [](char c) { return std::toupper(c); };
-  //   constexpr auto match = fmap(one_of("ab"), toupper);
-  //   THEN("it should upcase \"a\"") {
-  //     const auto result = match("a");
-  //     REQUIRE( result );
-  //     CHECK( get<0>(result.value()) == 'A' );
-  //   }
-  //   THEN("it should upcase \"b\"") {
-  //     const auto result = match("b");
-  //     REQUIRE( result );
-  //     CHECK( get<0>(result.value()) == 'B' );
-  //   }
-  //   THEN("it should NOT match \"c\"") {
-  //     CHECK( !match("c") );
-  //   }
-  // }
+  GIVEN("a character matcher [fmap(one_of(\"ab\"), toupper]") {
+    constexpr auto toupper = [](char c) { 
+      return ('a' <= c && c <= 'z') ? c + ('A' - 'a') : c;
+    };
+    constexpr auto match = fmap(toupper, one_of("ab"));
+    THEN("it should upcase \"a\"") {
+      constexpr auto result = match("a");
+      REQUIRE( result );
+      CHECK( result->first == 'A' );
+      CHECK( result->second == "" );
+    }
+    THEN("it should upcase \"b\"") {
+      constexpr auto result = match("b");
+      REQUIRE( result );
+      CHECK( result->first == 'B' );
+      CHECK( result->second == "" );
+    }
+    THEN("it should NOT match \"c\"") {
+      constexpr auto result = match("c");
+      CHECK( !result);
+    }
+    THEN("it should NOT match \"\"") {
+      constexpr auto result = match("");
+      CHECK( !result );
+    }
+  }
 
-  // GIVEN("a matcher [many(one_of(\"ab\"), 0, std::sum)]") {
-  //   constexpr auto sum = [](auto x, auto y) { return x + y; };
-  //   constexpr auto match = many(one_of("ab"), 0, sum);
-  //   THEN("it should sum \"aa\")") {
-  //     const auto result = match("aa");
-  //     REQUIRE( result );
-  //     CHECK( get<0>(result.value()) == ('a' + 'a') );
-  //   }
-  //   THEN("it should sum \"b\")") {
-  //     const auto result = match("b");
-  //     REQUIRE( result );
-  //     CHECK( get<0>(result.value()) == 'b' );
-  //   }
-  //   THEN("it should match sum \"\"") {
-  //     const auto result = match("");
-  //     REQUIRE( result );
-  //     CHECK( get<0>(result.value()) == 0 );
-  //   }
-  // }
+  GIVEN("a matcher [many(one_of(\"ab\"), 0, std::sum)]") {
+    constexpr auto sum = [](auto x, auto y) { return x + y; };
+    constexpr auto match = many(one_of("ab"), 0, sum);
+    THEN("it should sum \"aa\")") {
+      constexpr auto result = match("aa");
+      REQUIRE( result );
+      CHECK( result->first == ('a' + 'a') );
+      CHECK( result->second == "" );
+    }
+    THEN("it should sum \"b\")") {
+      constexpr auto result = match("b");
+      REQUIRE( result );
+      CHECK( result->first == 'b' );
+      CHECK( result->second == "" );
+    }
+    THEN("it should match sum \"\"") {
+      constexpr auto result = match("");
+      REQUIRE( result );
+      CHECK( result->first == 0 );
+      CHECK( result->second == "" );
+    }
+  }
 
-  // GIVEN("a matcher [many1(one_of(\"ab\"), 0, std::sum)]") {
-  //   constexpr auto sum = [](auto x, auto y) { return x + y; };
-  //   constexpr auto match = many1(one_of("ab"), 0, sum);
-  //   THEN("it should sum \"aa\")") {
-  //     const auto result = match("aa");
-  //     REQUIRE( result );
-  //     CHECK( get<0>(result.value()) == ('a' + 'a') );
-  //   }
-  //   THEN("it should sum \"b\")") {
-  //     const auto result = match("b");
-  //     REQUIRE( result );
-  //     CHECK( get<0>(result.value()) == 'b' );
-  //   }
-  //   THEN("it should NOT match sum \"\"") {
-  //     const auto result = match("");
-  //     CHECK( !result );
-  //   }
-  // }
-
+  GIVEN("a matcher [many1(one_of(\"ab\"), 0, sum)]") {
+    constexpr auto sum = [](auto x, auto y) { return x + y; };
+    constexpr auto match = many1(one_of("ab"), 0, sum);
+    THEN("it should sum \"aa\")") {
+      constexpr auto result = match("aa");
+      REQUIRE( result );
+      CHECK( result->first == ('a' + 'a') );
+      CHECK( result->second == "" );
+    }
+    THEN("it should sum \"b\")") {
+      constexpr auto result = match("b");
+      REQUIRE( result );
+      CHECK( result->first == 'b' );
+      CHECK( result->second == "" );
+    }
+    THEN("it should NOT match sum \"\"") {
+      constexpr auto result = match("");
+      CHECK( !result );
+    }
+  }
   
-  // GIVEN("a digits matcher [digits]") {
-  //   constexpr auto match = digits();
-  //   THEN("it should match \"0\"") {
-  //     const auto result = match("0");
-  //     REQUIRE( result );
-  //     CHECK( get<0>(result.value()) == 0 );
-  //   }
-  //   THEN("it should match \"012345\"") {
-  //     const auto result = match("012345");
-  //     REQUIRE( result );
-  //     CHECK( get<0>(result.value()) == 12345 );
-  //   }
-  //   THEN("it should NOT match \"\"") {
-  //     REQUIRE( !match("") );
-  //   }
-  //   THEN("it should NOT match \"a\"") {
-  //     REQUIRE( !match("a") );
-  //   }
-  // }
+  GIVEN("a matcher[bind(one_of(\"ab\"), one_of(\"cd\"))]") {
+    constexpr auto match = bind(one_of("ab"), [](char, parse_input_t rest) {
+	return one_of("cd")(rest);
+    });
+    THEN("it should match \"ac\"") {
+      constexpr auto result = match("ac");
+      REQUIRE( result );
+      CHECK( result->first == 'c' );
+      CHECK( result->second == "" );
+    }
+    THEN("it should match \"ad\"") {
+      constexpr auto result = match("ad");
+      REQUIRE( result );
+      CHECK( result->first == 'd' );
+      CHECK( result->second == "" );
+    }
+    THEN("it should match \"bc\"") {
+      constexpr auto result = match("bc");
+      REQUIRE( result );
+      CHECK( result->first == 'c' );
+      CHECK( result->second == "" );
+    }
+    THEN("it should match \"bd\"") {
+      constexpr auto result = match("bd");
+      REQUIRE( result );
+      CHECK( result->first == 'd' );
+      CHECK( result->second == "" );
+    }
+    THEN("it should NOT match \"a\"") {
+      constexpr auto result = match("a");
+      CHECK( !result );
+    }
+    THEN("it should NOT match \"be\"") {
+      constexpr auto result = match("be");
+      CHECK( !result );
+    }
+  } 
 
-  // GIVEN("an option parser [option('+', exactly('-'))]") {
-  //   constexpr auto match = option('+', exactly('-'));
-  //   THEN("it should return '+' if \"a\"") {
-  //     const auto result = match("a");
-  //     REQUIRE( result );
-  //     CHECK( get<0>(result.value()) == '+' );
-  //   }
-  //   THEN("it should return '-' if \"-\"") {
-  //     const auto result = match("-");
-  //     REQUIRE( result );
-  //     CHECK( get<0>(result.value()) == '-' );
-  //   }
-  // }
+  GIVEN("an option parser [option('+', make_char_parser('-'))]") {
+    constexpr auto match = option('+', make_char_parser('-'));
+    THEN("it should return '+' if \"a\"") {
+      constexpr auto result = match("a");
+      REQUIRE( result );
+      CHECK( result->first == '+' );
+      CHECK( result->second == "a" );
+    }
+    THEN("it should return '-' if \"-\"") {
+      constexpr auto result = match("-");
+      REQUIRE( result );
+      CHECK( result->first == '-' );
+      CHECK( result->second == "" );
+    }
+  }
 
-  // GIVEN("a number parser [number()]") {
-  //   constexpr auto match = number();
+  GIVEN("a matcher [lift('a')]") {
+    constexpr auto match = lift('a');
+    THEN("it should match \"xyz\"") {
+      constexpr auto result = match("xyz");
+      REQUIRE( result );
+      CHECK( result->first == 'a' );
+      CHECK( result->second == "xyz" );
+    }
+  }
 
-  // }
+  GIVEN("a matcher [combine(one_of(\"ab\"), one_of(\"bc\"), sum)]") {
+    constexpr auto match = combine(one_of("ab"), one_of("bc"), [](char a, char b) {
+	return a + b;
+    });
+    THEN("it should match \"ab\"") {
+      constexpr auto result = match("ab");
+      REQUIRE( result );
+      CHECK( result->first == ('a' + 'b') );
+      CHECK( result->second == "" );
+    }
+    THEN("it should match \"bb\"") {
+      constexpr auto result = match("bb");
+      REQUIRE( result );
+      CHECK( result->first == ('b' + 'b') );
+      CHECK( result->second == "" );
+    }
+    THEN("it should match \"ac\"") {
+      constexpr auto result = match("ac");
+      REQUIRE( result );
+      CHECK( result->first == ('a' + 'c') );
+      CHECK( result->second == "" );
+    }
+    THEN("it should match \"bc\"") {
+      constexpr auto result = match("bc");
+      REQUIRE( result );
+      CHECK( result->first == ('b' + 'c') );
+      CHECK( result->second == "" );
+    }
+    THEN("it should NOT match \"cb\"") {
+      constexpr auto result = match("cb");
+      CHECK( !result );
+    }
+    THEN("it should NOT match \"b\"") {
+      constexpr auto result = match("b");
+      CHECK( !result );
+    }
+    THEN("it should NOT match \"\"") {
+      constexpr auto result = match("");
+      CHECK( !result );
+    }
+  }
+
 }
